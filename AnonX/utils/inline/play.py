316f49c34fb6ad6e6a8d10469b10b98bc6d8a62c
Lucky Raja <129265896+LuckyRaja0@users.noreply.github.com>
2023-05-10 22:30:17 +0530

    Update play.py

--- AnonX/utils/inline/play.py
+++ AnonX/utils/inline/play.py
@@ -32,7 +32,7 @@ def stream_markup_timer(_, videoid, chat_id, played, dur):
     elif 80 <= anon < 95:
         bar = "————————◉—"
     else:
-        bar = "—————————◉"
+        bar = "——Jai——Shree——Ram———◉"
 
     buttons = [
         [
